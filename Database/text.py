9ee5957b1158fc455764762394a34de77d494039
bambooforest@gmail.com <bambooforest@gmail.com>
2020-03-12 09:52:20 +0100

    fixes metadata in files and adds initial work on database creation

---
+++ Database/text.py
@@ -0,0 +1,49 @@
+""" Text object for the 100 LC files """
+
+import pathlib
+
+ENCODING = "UTF-8"
+
+class Text:
+
+    @classmethod
+    def from_rglob(cls, dir, glob):
+        dir = pathlib.Path(dir)
+        return map(cls.from_file, dir.rglob(glob))
+
+    @classmethod
+    def from_file(cls, filename):
+        metadata = {}
+        with filename.open(encoding=ENCODING) as f:
+            for line in f:
+                if line.startswith("#"):
+                    line = line[1:].strip()
+                    key, sep, value = line.partition(":")
+                    key, value = (x.strip() for x in (key, value))
+                    assert key and sep
+                    metadata[key] = value
+                else:
+                    break
+            body = (line + f.read()).strip()
+        return cls(filename, metadata, body)
+
+    def __init__(self, filename, metadata, body):
+        self.filename = filename
+        self.metadata = metadata
+        self.body = body
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}({str(self.filename)!r})'
+
+    def to_file(self):
+        with self.filename.open("w", encoding=ENCODING) as f:
+            for k, v in self.metadata.items():
+                # If value is empty, replace it with the string "NA"
+                if not v.strip():
+                    v = "NA"
+                # Remove parentheses and minus symbol in metadata keys
+                k = k.replace("(", "")
+                k = k.replace(")", "")
+                k = k.replace("ISO_639-3", "ISO_6393")
+                f.write(f"# {k}:\t{v}\n")
+            f.write(f"\n{self.body}")
