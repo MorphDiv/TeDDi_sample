7ecc2addfba6aec2af8b58f95d924060cc39e80c
avonizos <oa.sozinova@gmail.com>
2020-03-20 00:18:40 +0100

    updated gutenberg_crawler.py

--- Crawlers/gutenberg_crawler.py
+++ Crawlers/gutenberg_crawler.py
@@ -32,23 +32,23 @@ warnings.filterwarnings("ignore")
 report = codecs.open('report_gutenberg.csv', 'w', 'utf-8')
 report.write('folder_name;tokens;txt_files\n')
 
-link = re.compile('<a href="(/ebooks.*?)">')
+link = re.compile('<li class="pgdbetext"><a href="(/ebooks.*?)">')
 p_tag = re.compile(u'<p.*?>(.*?)</p>', re.DOTALL)
 
+# Last element: languages in Gutenberg
 lang_dic = {
-    'en': ['English_eng', 'eng', 'English', 'English', 'Latn'],
-    'fi': ['Finnish_fin', 'fin', 'Finnish', 'Finnish', 'Latn'],
-    'fr': ['French_fra', 'fra', 'French', 'French', 'Latn'],
-    'de': ['German_deu', 'deu', 'German', 'German', 'Latn'],
-    'el': ['Greek_Modern_ell', 'ell', 'Greek (Modern)', 'Modern Greek', 'Grek'],
-    'he': ['Hebrew_Modern_heb', 'heb', 'Hebrew (Modern)', 'Modern Hebrew', 'Hebr'],
-    'ja': ['Japanese_jpn', 'jpn', 'Japanese', 'Japanese', 'Jpan'],
-    'ko': ['Korean_kor', 'kor', 'Korean', 'Korean', 'Kore'],
-    'zh': ['Mandarin_cmn', 'cmn', 'Mandarin', 'Mandarin Chinese', 'Hans'],
-    'fa': ['Persian_pes', 'pes', 'Persian', 'Western Farsi', 'Arab'],
-    'ru': ['Russian_rus', 'rus', 'Russian', 'Russian', 'Cyrl'],
-    'es': ['Spanish_spa', 'spa', 'Spanish', 'Spanish', 'Latn'],
-    'tl': ['Tagalog_tgl', 'tgl', 'Tagalog', 'Tagalog', 'Latn']
+     'en': ['English_eng', 'eng', 'English', 'English', 'Latn', 'English'],
+     'fi': ['Finnish_fin', 'fin', 'Finnish', 'Finnish', 'Latn', 'Finnish'],
+     'fr': ['French_fra', 'fra', 'French', 'French', 'Latn', 'French'],
+     'de': ['German_deu', 'deu', 'German', 'German', 'Latn', 'German'],
+     'el': ['Greek_Modern_ell', 'ell', 'Greek (Modern)', 'Modern Greek', 'Grek', 'Greek'],
+     'he': ['Hebrew_Modern_heb', 'heb', 'Hebrew (Modern)', 'Modern Hebrew', 'Hebr', 'Hebrew'],
+     'ja': ['Japanese_jpn', 'jpn', 'Japanese', 'Japanese', 'Jpan', 'Japanese'],
+     'zh': ['Mandarin_cmn', 'cmn', 'Mandarin', 'Mandarin Chinese', 'Hans', 'Chinese'],
+     'fa': ['Persian_pes', 'pes', 'Persian', 'Western Farsi', 'Arab', 'Farsi'],
+     'ru': ['Russian_rus', 'rus', 'Russian', 'Russian', 'Cyrl', 'Russian'],
+     'es': ['Spanish_spa', 'spa', 'Spanish', 'Spanish', 'Latn', 'Spanish'],
+     'tl': ['Tagalog_tgl', 'tgl', 'Tagalog', 'Tagalog', 'Latn', 'Tagalog'],
 }
 
 
@@ -86,8 +86,15 @@ def html_entities_transform(text):
 
 
 # Request HTML page for the current language
-def request(language):
+def request_lang(language):
     r = requests.get('http://www.gutenberg.org/browse/languages/' + language)
+    html = r.text
+    return html
+
+
+# Request HTML page for the current author
+def request_author(author):
+    r = requests.get('http://www.gutenberg.org/browse/authors/' + author)
     html = r.text
     return html
 
@@ -102,8 +109,26 @@ def find_links(html):
 # Get html with a book
 def get_book(cur_link):
     r = requests.get('http://www.gutenberg.org' + cur_link + '.html.noimages')
+    r.encoding = r.apparent_encoding
     html_book = r.text
-    return html_book
+
+    if 'Page Not Found' in html_book:
+        cur_link = cur_link.replace('ebooks', 'files')
+        find_number = re.search('[0-9]+', cur_link)
+        if find_number:
+            number = find_number.group(0)
+
+            print('\nhttp://www.gutenberg.org' + cur_link + '/' + number + '-h/' + number + '-h.htm')
+
+            r = requests.get('http://www.gutenberg.org' + cur_link + '/' + number + '-h/' + number + '-h.htm')
+            r.encoding = r.apparent_encoding
+            html_book = r.text
+
+            return html_book, 'http://www.gutenberg.org' + cur_link + '/' + number + '-h/' + number + '-h.htm'
+
+    print('\nhttp://www.gutenberg.org' + cur_link + '.html.noimages')
+
+    return html_book, 'http://www.gutenberg.org' + cur_link + '.html.noimages'
 
 
 # Find p tags
@@ -133,9 +158,9 @@ def find_p(html_book):
 
     for i in range(len(all_p)):
         if start_i < i < end_i:
-            text += all_p[i]
+            text += all_p[i] + '\n'
 
-    all_p = all_p[start_i + 1:end_i]
+    all_p = all_p[start_i+1:end_i]
 
     text = text.replace('<br/>', '')
 
@@ -158,7 +183,7 @@ def count_tokens(text, lang):
 
 
 # Make meta info
-def make_meta(lang, html_book, sample_type, cur_link):
+def make_meta(lang, html_book, sample_type, source):
     year = 'NA'
     find_year = re.search('<(.*?)name="DCTERMS.created"(.*?)>', html_book)
     if find_year:
@@ -199,11 +224,30 @@ def make_meta(lang, html_book, sample_type, cur_link):
                     short_description = author + ', ' + title + ', ' + content.group(1)
                 else:
                     short_description = content.group(1)
+
     else:
         if author != '' and title != '':
             short_description = author + ', ' + title
+
         else:
             short_description = 'Gutenberg Project, ' + lang_dic[lang][3]
+
+    # Try to find info in the tag pre
+    if year == 'NA' or short_description == 'Gutenberg Project, ' + lang_dic[lang][3]:
+        find_pre = re.search('<pre(.*?)>(.*?)</pre>', html_book, re.DOTALL)
+        if find_pre:
+
+            short_description = ''
+            find_author = re.search('[aA]uthor: (.+)\r\n', html_book)
+            find_title = re.search('[tT]itle: (.+)\r\n', html_book)
+            find_date = re.search('[rR]elease [dD]ate:(.*?)([0-9]{4})', html_book)
+
+            if find_author:
+                short_description += find_author.group(1)
+            if find_title:
+                short_description += ', ' + find_title.group(1)
+            if find_date:
+                year = find_date.group(2)
 
     short_description = html_entities_transform(short_description)
 
@@ -222,7 +266,7 @@ def make_meta(lang, html_book, sample_type, cur_link):
 # writing_system:	''' + lang_dic[lang][4] + '''
 # special_characters:	NA
 # short_description:	''' + html_entities_transform(short_description) + '''
-# source:	''' + 'http://www.gutenberg.org' + cur_link + '.html.noimages' + '''
+# source:	''' + source + '''
 # copyright_short:	http://www.gutenberg.org
 # copyright_long:	http://www.gutenberg.org
 # sample_type:	''' + sample_type + '''
@@ -247,17 +291,21 @@ def one_sample(lines, lang):
     else:
         search_string = '^.{0,10}$'
 
-    for i in range(len(lines) - 1):
+    for i in range(len(lines)-1):
         # Random start -- visual division of the text
         # E.g. blank line, very short line (sample after that)
         find_start = re.search(search_string, lines[i])
-        if find_start and lines[i + 1][0] not in '。！」？、，' \
-                and lines[i + 1][0] not in punctuation:
-            starts.append(i)
+        if find_start:
+            try:
+                if lines[i+1][0] not in '。！」？、，'\
+                        and lines[i+1][0] not in punctuation:
+                    starts.append(i)
+            except:
+                starts.append(i)
 
     # Choose random start from collected ids (and 0 line)
     random_start = random.choice(starts)
-    print('RANDOM START (LINE): ', random_start + 1)
+    print('RANDOM START (LINE): ', random_start+1)
 
     # Take out 50000 tokens starting from the random start
     line_counter = 0
@@ -299,6 +347,7 @@ def one_sample(lines, lang):
 
 
 def sampling(text, tokens, lang):
+
     current_tokens = 0
 
     samples = []
@@ -310,12 +359,12 @@ def sampling(text, tokens, lang):
         # No constraints on the number of samples per file (until possible to gather contiguous 50000 tokens)
 
         # Number of samples
-        num_samples = int(tokens / 50000)
+        num_samples = int(tokens/50000)
 
         print('NUM SAMPLES: ', num_samples)
 
         for n in range(num_samples):
-            print('SAMPLE #' + str(n + 1))
+            print('SAMPLE #' + str(n+1))
             lines, current_sample, current_sample_tokens = one_sample(lines, lang)
             samples.append(current_sample)
             current_tokens += current_sample_tokens
@@ -328,54 +377,94 @@ def sampling(text, tokens, lang):
 
 
 def main():
+    # tgl 15, 17, 18, 25, 51 -- to check
+    # added manually
+    # links = ['/ebooks/14205',
+    #          '/ebooks/18536',
+    #          '/ebooks/16446',
+    #          '/ebooks/18888',
+    #          '/ebooks/18887']
+
+    # fr 88, 94, 106 -- to check
+    # links = ['/ebooks/24300',
+    #          '/ebooks/29251',
+    #          '/ebooks/22388']
+
     for lang in lang_dic:
+        sources = []
 
         print('\nLANGUAGE: ', lang_dic[lang][2])
 
         total_tokens = 0
         txt_files = 0
 
-        html = request(lang)
-        found_links = find_links(html)
+        if lang == 'en':
+            authors = 'abcdefghijklmnopqrstuvwxyz'
+            for letter in authors:
+                html = request_author(letter)
+                en_link = re.compile('<li class="pgdbetext"><a href="(/ebooks.*?)">.*?English')
+                found_links = re.findall(en_link, html)
+                print('NUM LINKS: ', len(found_links))
+
+        else:
+            html = request_lang(lang)
+            found_links = find_links(html)
+
         current_counter = 0
+
         for fl in found_links:
 
             if total_tokens < 5000000:
 
-                print('\nhttp://www.gutenberg.org' + fl + '.html.noimages')
-                html_book = get_book(fl)
-                text, all_p = find_p(html_book)
-                tokens = count_tokens(text, lang)
-                print('TOKENS: ', tokens)
+                html_book, source = get_book(fl)
 
-                samples, current_tokens = sampling(text, tokens, lang)
+                # Check language of the file (should be one)
+                if 'Language: ' + lang_dic[lang][5] in html_book:
 
-                total_tokens += current_tokens
+                    if source not in sources:
+                        sources.append(source)
 
-                if tokens > 50000:
-                    sample_type = 'part'
-                else:
-                    sample_type = 'whole'
+                        text, all_p = find_p(html_book)
+                        text = html_entities_transform(text)
 
-                meta = make_meta(lang, html_book, sample_type, fl)
+                        if text != '':
 
-                for sample in samples:
-                    fname, current_counter = generate_fname(lang, current_counter)
-                    f = codecs.open(fname, 'w', 'utf-8')
-                    f.write(meta)
+                            tokens = count_tokens(text, lang)
+                            print('TOKENS: ', tokens)
 
-                    # Replace tabs
-                    sample = re.sub('\t+', ' ', sample)
+                            samples, current_tokens = sampling(text, tokens, lang)
 
-                    f.write(sample)
-                    txt_files += 1
+                            total_tokens += current_tokens
 
-                print('CURRENT TOTAL TOKENS: ', total_tokens)
+                            if tokens > 50000:
+                                sample_type = 'part'
+                            else:
+                                sample_type = 'whole'
+
+                            meta = make_meta(lang, html_book, sample_type, source)
+
+                            for sample in samples:
+                                fname, current_counter = generate_fname(lang, current_counter)
+                                f = codecs.open(fname, 'w', 'utf-8')
+                                f.write(meta)
+
+                                # Replace tabs
+                                sample = re.sub('\t+', ' ', sample)
+
+                                f.write(sample)
+
+                                txt_files += 1
+
+                            print('CURRENT TOTAL TOKENS: ', total_tokens)
 
             else:
                 print('TOTAL TOKENS: ', total_tokens)
                 report.write(lang_dic[lang][0] + ';' + str(total_tokens) + ';' + str(txt_files) + '\n')
                 break
+
+        if total_tokens < 5000000:
+            print('TOTAL TOKENS: ', total_tokens)
+            report.write(lang_dic[lang][0] + ';' + str(total_tokens) + ';' + str(txt_files) + '\n')
 
 
 if __name__ == "__main__":
