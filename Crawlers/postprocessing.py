f9f7d0c100941058e1c92f9e7e074ac741005084
avonizos <oa.sozinova@gmail.com>
2020-03-31 21:38:00 +0200

    Resolved conflicts

--- Crawlers/postprocessing.py
+++ Crawlers/postprocessing.py
@@ -3,7 +3,6 @@ Postprocessing of the files crawled from Gutenberg and OpenSubtitles:
     Fixes meta (tab after each tag; replace multiple tabs and spaces)
     Fixes broken lines in meta
     Replaces tabs and spaces to a single space in body
-
 """
 
 import os
@@ -15,46 +14,56 @@ root_path = '../Corpus'
 
 for root, dirs, files in os.walk(root_path):
     for file in files:
-        f = codecs.open(os.path.join(root, file), 'r', 'utf-8')
-        text = f.readlines()
-        fixed_text = []
+        with codecs.open(os.path.join(root, file), 'r', 'utf-8') as f:
+            text = f.readlines()
+            fixed_text = []
 
-        # Change only crawled from Gutenberg and OpenSubtitles
-        if 'gutenberg' in text[12] or 'gutenberg' in text[14] \
-                or 'opensubtitles' in text[12]:
-            print(file)
-            i = 0
-            while i < len(text):
-                line = text[i]
-                if line.startswith('#'):
+            # Change only crawled from Gutenberg and OpenSubtitles
+            if 'gutenberg' in text[12] or 'gutenberg' in text[14] \
+                    or 'opensubtitles' in text[12]:
+                print(file)
+                i = 0
+                while i < len(text):
+                    line = text[i]
+                    if line.startswith('#'):
 
-                    # Replace space(s) to a single tab in meta
-                    line = re.sub('(.*?): +', '\g<1>:\t', line)
+                        # Replace space(s) to a single tab in meta
+                        line = re.sub('(.*?): +', '\\g<1>:\t', line)
 
-                    # Replace multiple tabs to a single tab in meta
-                    line = re.sub('\t{2,}', '\t', line)
+                        # Replace multiple tabs to a single tab in meta
+                        line = re.sub('\t{2,}', '\t', line)
 
-                    # Fix broken lines
-                    if 'short_description' in line:
-                        if text[i+1].startswith('\n'):
-                            line = text[i].strip() + ', ' + text[i+2]
-                            i += 2
+                        # Fix broken lines
+                        if 'short_description' in line:
+                            if text[i+1].startswith('\n'):
+                                j = 1
+                                line = text[i].strip()
+                                while not text[i+j].startswith('#'):
+                                    if text[i+j] == '\n':
+                                        line += ','
+                                    else:
+                                        line += ' ' + text[i+j].strip()
+                                    j += 1
 
-                    # Replace tab in copyright_long (OpenSubtitles)
-                    line = re.sub('\t+Extracting', ' Extracting', line)
+                                line += '\n'
+                                i += j
 
-                    # Replace multiple spaces
-                    line = re.sub(' {2,}', ' ', line)
-                    fixed_text.append(line)
+                        # Replace tab in copyright_long (OpenSubtitles)
+                        line = re.sub('\t+Extracting', ' Extracting', line)
 
-                else:
+                        # Replace multiple spaces
+                        line = re.sub(' {2,}', ' ', line)
+                        fixed_text.append(line)
 
-                    # Replace tabs and multiple spaces in body to a single space
-                    line = re.sub('( |\t)+', ' ', line)
-                    fixed_text.append(line)
+                    else:
 
-                i += 1
+                        # Replace tabs and multiple spaces in body to a single space
+                        line = re.sub('[ \t]+', ' ', line)
+                        fixed_text.append(line)
 
-            fixed_f = codecs.open(os.path.join(root, file), 'w', 'utf-8')
-            for line in fixed_text:
-                fixed_f.write(line)
+                    i += 1
+
+                with codecs.open(os.path.join(root, file), 'w', 'utf-8') as fixed_f:
+                    for line in fixed_text:
+                        fixed_f.write(line)
+
