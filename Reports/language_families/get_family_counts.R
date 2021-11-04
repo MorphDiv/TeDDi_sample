edb2ae8c227fc400d5e07ccf78592498af8165ab
bambooforest <bambooforest@gmail.com>
2021-11-04 09:13:29 +0100

    add bert files

--- Reports/language_families/get_family_counts.R
+++ Reports/language_families/get_family_counts.R
@@ -1,3 +1,11 @@
+---
+title: "Get language family counts for various multilingual resources"
+author: "Steven Moran"
+date: "(`r format(Sys.time(), '%d %B, %Y')`)"
+output:
+  github_document
+---
+
 library(tidyverse)
 
 # Get counts of families for 100LC
@@ -26,10 +34,38 @@ ud <- ud %>% mutate(`alpha3-b` = coalesce(`alpha3-b`, V1))
 ud <- ud %>% select(X1, V1, `alpha3-b`) %>% rename(file_prefix=X1, ISO6391 = V1, iso639P3code = `alpha3-b`)
 ud <- left_join(ud, glottolog)
 ud %>% filter(!is.na(family_id)) %>% select(family_id) %>% distinct()
+nrow(ud %>% filter(!is.na(family_id)) %>% select(family_id) %>% distinct())
+
+# Get mBERT language family counts
+mbert <- read_csv('mBERT.csv', col_names=F)
+
+# This gets ISO codes by language name
+library(lingtypology)
+mbert_iso <- as.data.frame(iso.lang(mbert$X1))
+
+# Reformat the rownames
+mbert_iso <- mbert_iso %>% rownames_to_column(var="Name") %>% rename(iso639P3code = `iso.lang(mbert$X1)`)
+
+# But doesn't get all of them, so we will have to fill in some by hand...
+write_csv(mbert_iso, 'mBERT_iso.csv')
+
+# Going through by hand, I randomly chose certain codes, e.g. Colloquiual Malay for "Malay", so that 
+# they could be linked easily to Glottlog. Note also, that mBERT languages include things like: 
+# Serbian, Bosnian, Croatian, Serbo-Croatian -- which by language code is simply "hbs". So the 
+# "language" coverage is actually less than what is reported.
+
+# Let's reload the by hand data
+mbert_iso <- read_csv('mBERT_iso_updated_by_hand.csv')
+
+library(testthat)
+expect_false(any(is.na(mbert_iso$iso639P3code)))
+
+# How many true languages?
+nrow(mbert_iso %>% select(iso639P3code) %>% distinct())
 
 
 
 
-# mBert
+
 
 
